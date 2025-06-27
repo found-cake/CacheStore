cd62f62aa186e282982d6b15d904ab49b63e16ed
found-cake <me@foundcake.kr>
2025-06-27 06:03:31 +0000

    feat: implement type validation in numberic typed getter/setter methods

--- store/type_uinteger.go
+++ store/type_uinteger.go
@@ -1,59 +1,31 @@
 package store
 
 import (
-	"encoding/binary"
 	"time"
 
-	"github.com/found-cake/CacheStore/errors"
+	"github.com/found-cake/CacheStore/store/types"
 )
 
 func (s *CacheStore) GetUInt16(key string) (uint16, error) {
-	data, err := s.Get(key)
-	if err != nil {
-		return 0, err
-	}
-	if len(data) < 2 {
-		return 0, errors.ErrInvalidDataLength(2, len(data))
-	}
-	return binary.LittleEndian.Uint16(data), nil
+	return s.getNum16(key, types.UINT16)
 }
 
 func (s *CacheStore) SetUInt16(key string, value uint16, exp time.Duration) error {
-	buffer := make([]byte, 2)
-	binary.LittleEndian.PutUint16(buffer, value)
-	return s.Set(key, buffer, exp)
+	return s.setNum16(key, types.UINT16, value, exp)
 }
 
 func (s *CacheStore) GetUInt32(key string) (uint32, error) {
-	data, err := s.Get(key)
-	if err != nil {
-		return 0, err
-	}
-	if len(data) < 4 {
-		return 0, errors.ErrInvalidDataLength(4, len(data))
-	}
-	return binary.LittleEndian.Uint32(data), nil
+	return s.getNum32(key, types.UINT32)
 }
 
 func (s *CacheStore) SetUInt32(key string, value uint32, exp time.Duration) error {
-	buffer := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buffer, value)
-	return s.Set(key, buffer, exp)
+	return s.setNum32(key, types.UINT32, value, exp)
 }
 
 func (s *CacheStore) GetUInt64(key string) (uint64, error) {
-	data, err := s.Get(key)
-	if err != nil {
-		return 0, err
-	}
-	if len(data) < 8 {
-		return 0, errors.ErrInvalidDataLength(8, len(data))
-	}
-	return binary.LittleEndian.Uint64(data), nil
+	return s.getNum64(key, types.UINT64)
 }
 
 func (s *CacheStore) SetUInt64(key string, value uint64, exp time.Duration) error {
-	buffer := make([]byte, 8)
-	binary.LittleEndian.PutUint64(buffer, value)
-	return s.Set(key, buffer, exp)
+	return s.setNum64(key, types.UINT64, value, exp)
 }
