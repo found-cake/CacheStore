802f6c252e665f2a31f8429adf12330ebaa495a7
found-cake <me@foundcake.kr>
2025-06-29 00:02:46 +0000

    feat(config): add DBSaveInterval field  for periodic database saves

--- config/config.go
+++ config/config.go
@@ -3,15 +3,17 @@ package config
 import "time"
 
 type Config struct {
-	GCInterval time.Duration
-	DBSave     bool
-	DBFileName string
+	GCInterval         time.Duration
+	DBSave             bool
+	DBFileName         string
+	DBAutoSaveInterval time.Duration
 }
 
 func DefaultConfig() Config {
 	return Config{
-		GCInterval: 10 * time.Second,
-		DBSave:     true,
-		DBFileName: "cache.db",
+		GCInterval:         10 * time.Second,
+		DBSave:             true,
+		DBFileName:         "cache.db",
+		DBSaveInterval: 10 * time.Minute,
 	}
 }
